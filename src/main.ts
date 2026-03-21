db4ab344106365cff5d1de327dd8ffba80b93216
samh7 <samh74490@gmail.com>
2026-03-21 18:23:19 +0000

    add openapi support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { writeFileSync } from 'fs';
 import { AppModule } from './AppModule';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('School Backend API')
+    .setDescription('The API documentation for a School Backend Api App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  writeFileSync('./swagger-spec.json', JSON.stringify(document));
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
